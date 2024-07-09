3cd5dbf3d28fba4e6d0626bc9a6d7e94565ba83c
amew0 <aminekidane4@gmail.com>
2024-07-09 11:05:41 +0400

    the ft saved is quantized

--- ft-medical.py
+++ ft-medical.py
@@ -6,7 +6,8 @@ import torch
 import transformers
 import huggingface_hub
 import wandb
-wandb.require('core')
+
+wandb.require("core")
 from datetime import datetime
 from dotenv import load_dotenv
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
@@ -15,13 +16,15 @@ from peft import LoraConfig, PeftModel
 from datasets import load_dataset
 from time import time
 
-cudas = os.getenv('CUDA_VISIBLE_DEVICES')
-if cudas: 
-    gpus = [torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())] 
-else: gpus = [torch.device('cuda')] 
+cudas = os.getenv("CUDA_VISIBLE_DEVICES")
+if cudas:
+    gpus = [torch.device(f"cuda:{i}") for i in range(torch.cuda.device_count())]
+else:
+    gpus = [torch.device("cuda")]
 
 gpu0, gpu1 = gpus[0], None
-if len(gpus) > 1: gpu1 = gpus[1]
+if len(gpus) > 1:
+    gpu1 = gpus[1]
 
 start = time()
 
@@ -42,7 +45,7 @@ torch.cuda.memory_summary(device=None, abbreviated=False)
 ME = "/dpc/kunf0097/l3-8b"
 RUN_ID = datetime.now().strftime("%y%m%d%H%M%S")
 logg(RUN_ID)
-logg('ft-medical.py')
+logg("ft-medical.py")
 
 # Initialize tokenizer
 name = "meta-llama/Meta-Llama-3-8B-Instruct"
@@ -52,24 +55,24 @@ bnb_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_quant_type="nf4",
     bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_use_double_quant=True
+    bnb_4bit_use_double_quant=True,
 )
 model = AutoModelForCausalLM.from_pretrained(
-    name, 
+    name,
     cache_dir=f"{ME}/l3-8b/model",
     quantization_config=bnb_config,
     torch_dtype=torch.float16,
     device_map={"": 0},
     low_cpu_mem_usage=True,
-    return_dict=True
+    return_dict=True,
 )
 
 tokenizer = AutoTokenizer.from_pretrained(
-    name, 
-    cache_dir=f"{ME}/l3-8b/tokenizer", 
-    padding_side="right", 
+    name,
+    cache_dir=f"{ME}/l3-8b/tokenizer",
+    padding_side="right",
     pad_token_id=(0),
-    legacy=False
+    legacy=False,
 )
 tokenizer.pad_token = tokenizer.eos_token
 
@@ -85,14 +88,16 @@ peft_config = LoraConfig(
 )
 
 # Tokenize prompt function
-cutoff_len=256 # most important hp to control CUDA OOM
+cutoff_len = 256  # most important hp to control CUDA OOM
+
+
 def tokenize(prompt, tokenizer, add_eos_token=True):
     result = tokenizer(
         prompt,
         truncation=True,
         max_length=cutoff_len,  # Use the cutoff_len variable you've defined
         padding="max_length",  # Ensure padding is done to the max_length
-        return_tensors="pt",   # Return PyTorch tensors
+        return_tensors="pt",  # Return PyTorch tensors
     )
     result["input_ids"] = result["input_ids"].flatten()
     result["attention_mask"] = result["attention_mask"].flatten()
@@ -105,27 +110,31 @@ def tokenize(prompt, tokenizer, add_eos_token=True):
     result["labels"] = result["input_ids"].clone()  # Clone input_ids for labels
     return result
 
+
 def generate_and_tokenize_prompt(data_point):
     tokenized_full_prompt = tokenize(data_point["prompt"], tokenizer=tokenizer)
-    if gpu1: tokenized_full_prompt.to(gpu1)
+    if gpu1:
+        tokenized_full_prompt.to(gpu1)
     return tokenized_full_prompt
 
+
 # Load and process dataset
-data_path = './data/1/medical.json'
-output_dir = f'{ME}/output/output_{RUN_ID}'
+data_path = "./data/1/medical.json"
+output_dir = f"{ME}/output/output_{RUN_ID}"
 data = load_dataset("json", data_files=data_path)
 train_dataset = data["train"].shuffle().map(generate_and_tokenize_prompt)
 
-eval_data_path = './data/1/eval_medical_2k.json'
+eval_data_path = "./data/1/eval_medical_2k.json"
 eval_data = load_dataset("json", data_files=eval_data_path)
 eval_dataset = data["train"].shuffle().map(generate_and_tokenize_prompt)
+
 
 # Build Trainer
 def build_trainer(tokenizer=tokenizer, model=model):
     train_args = transformers.TrainingArguments(
         per_device_train_batch_size=2,
         gradient_accumulation_steps=4,
-        eval_accumulation_steps=1, # !very important to send data to cpu
+        eval_accumulation_steps=1,  # !very important to send data to cpu
         warmup_steps=1,
         num_train_epochs=10,
         learning_rate=3e-4,
@@ -134,7 +143,7 @@ def build_trainer(tokenizer=tokenizer, model=model):
         optim="adamw_torch",
         output_dir=output_dir,
         group_by_length=False,
-        dataloader_drop_last=False
+        dataloader_drop_last=False,
     )
     trainer = SFTTrainer(
         model=model,
@@ -146,6 +155,7 @@ def build_trainer(tokenizer=tokenizer, model=model):
         max_seq_length=cutoff_len,
     )
     return trainer
+
 
 trainer = build_trainer()
 # Train model
@@ -163,21 +173,21 @@ trainer.model.save_pretrained(model_save_path)
 
 # saving to load later from https://www.youtube.com/watch?v=Pb_RGAl75VE&ab_channel=DataCamp
 model = AutoModelForCausalLM.from_pretrained(
-    name, 
+    name,
     cache_dir=f"{ME}/l3-8b/model",
     torch_dtype=torch.float16,
     device_map={"": 0},
-    return_dict=True
+    return_dict=True,
 )
-model = PeftModel.from_pretrained(model,model_save_path)
+model = PeftModel.from_pretrained(model, model_save_path)
 model = model.merge_and_unload()
 
 tokenizer = AutoTokenizer.from_pretrained(
-    name, 
-    cache_dir=f"{ME}/l3-8b/tokenizer", 
-    padding_side="right", 
+    name,
+    cache_dir=f"{ME}/l3-8b/tokenizer",
+    padding_side="right",
     pad_token_id=(0),
-    legacy=False
+    legacy=False,
 )
 tokenizer.pad_token = tokenizer.eos_token
 
